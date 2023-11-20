135fdc38d4fcac48015c9f839ab02205950d5d8d
FelipeAAug <145213036+FelipeAAug@users.noreply.github.com>
2023-11-19 21:05:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome: ")
+let ano = Number(prompt("digite seu ano de nascimento: "))
+let anoatual = 2023
+let idade = anoatual - ano
+
+document.write(`Olá, ${nome}, você tem ${idade} anos!`)
